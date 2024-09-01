a0dd3da3089d0d523dc2f6e7e4569ca42af2d75f
Scrying-Circle <allowed401@gmail.com>
2024-09-01 13:46:21 +1200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io::Cursor;
+
+use arboard::{Clipboard, ImageData, SetExtLinux};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "")]
+    image_path: String,
+
+    #[arg(short, long, default_value = "")]
+    text_path: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if !args.image_path.is_empty() {
+        let decoded_image = image::DynamicImage::from_decoder(image::codecs::png::PngDecoder::new(Cursor::new(std::fs::read(&args.image_path).unwrap().as_slice())).unwrap()).unwrap();
+
+        let image_data = ImageData {
+            width: decoded_image.width() as usize,
+            height: decoded_image.height() as usize,
+            bytes: decoded_image.as_bytes().into(),
+        };
+
+        if cfg!(target_os = "linux") {
+            Clipboard::new().unwrap().set().wait().image(image_data).unwrap();
+        } else {
+            Clipboard::new().unwrap().set_image(image_data).unwrap();
+        }
+    } else if !args.text_path.is_empty() {
+        let text = std::fs::read_to_string(args.text_path).unwrap();
+        if cfg!(target_os = "linux") {
+            Clipboard::new().unwrap().set().wait().text(text).unwrap();
+        } else {
+            Clipboard::new().unwrap().set_text(text).unwrap();
+        }
+    };
+}
